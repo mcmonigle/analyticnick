ec3bf3e2173bf3121763fc166038a4c187ecb202
Nicholas McMonigle <41704438+mcmonigle@users.noreply.github.com>
2021-10-06 16:55:47 -0400

    attempt3 to fix blog meta.json file location

--- src/Blog/BlogHome.js
+++ src/Blog/BlogHome.js
@@ -7,7 +7,21 @@ import {
     Link
 } from "react-router-dom";
 import bp1img from './Blogs/BlogPost1/img/roc.png'
-const bp1 = require('Blogs/BlogPost1/meta.json')
+const bp1meta = {   "title": "This is the title ",
+                    "date": "May 16th, 2021",
+                    "image": "./BlogPost1/img/roc.png",
+                    "link": "/blog/blog1",
+                    "imageTitle": "image title",
+                    "tags": [{
+                        "label": "MIMIC-III",
+                        "description": "MIMIC-III is a publicly-available dataset consisting of tens of thousands of records of longitudinal EMR data.",
+                        "color": "blue"
+                    },
+                    {   "label": "Machine learning",
+                        "description": "In this post I use machine learning",
+                        "color": "green"
+                    }]}
+// require('./Blogs/BlogPost1/meta.json')
 
 const useStyles = makeStyles((theme) => ({
 
@@ -34,7 +48,7 @@ export default function BlogHome() {
               social={sidebar.social}
               />            
               <Grid item xs={12} md={8}>
-                <ProjectPreview post={bp1} img={bp1img} />
+                <ProjectPreview post={bp1meta} img={bp1img} />
               </Grid>
           </Grid>
 }
