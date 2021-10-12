43a3afc1f35ae0e8520f2c8175a3f2abd251ff4b
Nicholas McMonigle <41704438+mcmonigle@users.noreply.github.com>
2021-10-12 13:55:50 -0400

    favicon credit on about page

--- src/About.js
+++ src/About.js
@@ -1,4 +1,6 @@
 
 export default function About() {
-    return <main><div>I'm Nick.</div></main>
+    return <main>
+        <div>I'm Nick.</div>
+        <p><a href='https://xkcd.com/979' style={{color: 'rgb(136, 190, 233)', textDecoration: 'none'}}>Favicon derived from XKCD</a></p></main>
 }
