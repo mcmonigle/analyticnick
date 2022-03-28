5ff814def25b2b13cac1edcd9b18994259e997ed
Nicholas McMonigle <41704438+mcmonigle@users.noreply.github.com>
2022-03-28 11:39:48 -0400

    updated about me

--- src/About.js
+++ src/About.js
@@ -7,15 +7,16 @@ export default function About() {
         return <div>
         <h1>Hi! I'm Nick.</h1>
         <p>I'm a Data Scientist and Machine Learning Engineer with a variety of interests.</p>
-        <p>I have a B.S. with majors in Statistics and Mathematics from the University of Minnesota, 
-            and I have a professional background in data science consulting, web application development,
-            economic research, military operations, and intelligence.</p>
+        <p>I have a B.S. with majors in Statistics and Mathematics from the University of Minnesota 
+            and I'm currently pursuing a M.E. in Systems Engineering from the University of Virginia.
+            I have a professional background in data science, web application development,
+            economic research, defense, and intelligence.</p>
         <p>I'm from Minnesota and live in Charlottesville, VA, with my wife, Megan, and our two dogs.</p>
         <p>I built this site using React and a 
             <a style={{color: 'rgb(136, 190, 233)', textDecoration: 'none'}} href='https://mui.com/getting-started/templates/'> modified MaterialUI template</a>. 
-            This site's static files are served with AWS (S3, Cloudfront). The favicon is derived from a 
+            This site's static files are served with AWS (S3 + Cloudfront). The favicon is derived from a 
         <a href='https://xkcd.com/979' style={{color: 'rgb(136, 190, 233)', textDecoration: 'none'}}> timeless XKCD comic</a>.</p>
-        <p>Please add/message me <a href='https://www.linkedin.com/in/nicholasmcmonigle' style={{color: 'rgb(136, 190, 233)', textDecoration: 'none'}}>on LinkedIn</a> if you'd like to contact me.</p>
+        <p>Please contact me at "lastname.dev@gmail.com" where lastname=mcmonigle, or message me <a href='https://www.linkedin.com/in/nicholasmcmonigle' style={{color: 'rgb(136, 190, 233)', textDecoration: 'none'}}>on LinkedIn</a>.</p>
         <p>Thanks for visiting!</p>
     </div>
     }
